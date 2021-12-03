853e7e818a5f18b88b7a89e642d52a852888e08b
Mohi Uddin Mahim <mohi35-2790@diu.edu.bd>
2021-12-03 11:56:42 +0600

    landing page structure created

---
+++ src/components/LandingPage/LandingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LandingHeader from '../LandingHeader/LandingHeader';
+import Login from '../Login/Login';
+
+const LandingPage = () => {
+    return (
+        <div>
+            <LandingHeader></LandingHeader>
+            <Login></Login>
+        </div>
+    );
+};
+
+export default LandingPage;
