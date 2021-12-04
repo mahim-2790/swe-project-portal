bc0a278949c3f7fe3ef668658c156783e51f1a88
Mohi Uddin Mahim <mohi35-2790@diu.edu.bd>
2021-12-04 22:59:08 +0600

    landing page nav created

--- src/components/LandingPage/LandingHeader/LandingHeader.js
+++ src/components/LandingPage/LandingHeader/LandingHeader.js
@@ -1,9 +1,24 @@
+import { AppBar, Container, Toolbar, Typography } from '@mui/material';
 import React from 'react';
 
 const LandingHeader = () => {
     return (
         <div>
-
+            <AppBar position="static">
+                <Container maxWidth="xl">
+                    <Toolbar disableGutters>
+                        <img src={""} alt="logo" />
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="div"
+                            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+                        >
+                            SWE Project Portal
+                        </Typography>
+                    </Toolbar>
+                </Container>
+            </AppBar>
         </div>
     );
 };
