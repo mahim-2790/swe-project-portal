11f7acc0aca18a36ae9eb97ea5c5ea2f8251daec
dipta-2707 <dipta35-2707@diu.edu.bd>
2021-12-21 00:48:53 +0600

    registration ui fixed

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 import { NavLink, useHistory } from 'react-router-dom';
 import useAuth from './../../../hooks/useAuth';
 import LandingHeader from '../LandingHeader/LandingHeader';
+import Grid from "@mui/material/Grid";
+import illastrateRegistration from "../../../Asset/images/registration_undraw.svg"
 
 const Register = () => {
     const [loginData, setLoginData] = useState({});
@@ -16,6 +18,9 @@ const Register = () => {
         newLoginData[field] = value;
         setLoginData(newLoginData);
     }
+    const controlRadio = e =>{
+        
+    }
     const handleOnSubmit = e => {
         if (loginData.password !== loginData.confirmPassword) {
             alert('Your password did not match');
@@ -27,80 +32,102 @@ const Register = () => {
     return (
         <div>
             <LandingHeader></LandingHeader>
-            <Container>
-                <Typography variant="h4" component="h2" sx={{ fontWeight: 'medium', my: 3 }}>
+            <Container style={{paddingTop:"5"}}>
+               
+                <Grid container spacing={2}>
+                    <Grid item xs={12} sm={6} style={{display:"flex", justifyContent:"center",alignItems:"center"}}>
+                        <img src={illastrateRegistration} style={{ width: "90%"}}></img>
+                    </Grid>
+                    
+                    <Grid item  xs={12} sm={6}>
+                    <Typography variant="h4" component="h2" sx={{ fontWeight: 'medium', my: 3 }}>
                     Registration
-                </Typography>
-                <form onSubmit={handleOnSubmit} >
-                    <TextField
-                        id="name"
-                        name="name"
-                        label="Your Name"
-                        variant="standard"
-                        sx={{ width: "20%", mt: 2 }}
-                        onBlur={handleOnBlur}
-                    />
-                    <br />
-                    <TextField
-                        id="id"
-                        name="id"
-                        label="ID"
-                        variant="standard"
-                        sx={{ width: "20%", mt: 2 }}
-                        onBlur={handleOnBlur}
-                    />
-                    <br />
-                    <TextField
-                        id="email"
-                        name="email"
-                        label="Email"
-                        variant="standard"
-                        type="email"
-                        sx={{ width: "20%", mt: 2 }}
-                        onBlur={handleOnBlur}
-                    />
-                    <br />
-                    <TextField
-                        id="password"
-                        name="password"
-                        label="Password"
-                        variant="standard"
-                        type="password"
-                        sx={{ width: "20%", mt: 2 }}
-                        onBlur={handleOnBlur}
-                    />
-                    <br />
+                     </Typography>
+                    <form onSubmit={handleOnSubmit} >
+                   
+                   <TextField
+                       id="name"
+                       name="name"
+                       label="Your Name"
+                       variant="standard"
+                       sx={{ width: "90%", mt: 2 }}
+                       onBlur={handleOnBlur}
+                   />
+                   <br />
+                   <TextField
+                       id="id"
+                       name="id"
+                       label="ID"
+                       variant="standard"
+                       sx={{ width: "90%", mt: 2 }}
+                       onBlur={handleOnBlur}
+                   />
+                   <br />
+                   <TextField
+                       id="email"
+                       name="email"
+                       label="Email"
+                       variant="standard"
+                       type="email"
+                       sx={{ width: "90%", mt: 2 }}
+                       onBlur={handleOnBlur}
+                   />
+                   <br />
+                   <TextField
+                       id="password"
+                       name="password"
+                       label="Password"
+                       variant="standard"
+                       type="password"
+                       sx={{ width: "90%", mt: 2 }}
+                       onBlur={handleOnBlur}
+                   />
+                   <br />
 
-                    <TextField
-                        id="confirm-password"
-                        name="confirmPassword"
-                        label="Confirm Password"
-                        variant="standard"
-                        type="password"
-                        sx={{ width: "20%", mt: 2 }}
-                        onBlur={handleOnBlur}
-                    />
-                    <br />
+                   <TextField
+                       id="confirm-password"
+                       name="confirmPassword"
+                       label="Confirm Password"
+                       variant="standard"
+                       type="password"
+                       sx={{ width: "90%", mt: 2 }}
+                       onBlur={handleOnBlur}
+                   />
+                   <br />
 
-                    <FormControl component="fieldset" sx={{ width: "20%", mt: 3, textAlign: 'left' }} onBlur={handleOnBlur}>
-                        <FormLabel component="legend">Role</FormLabel>
-                        <RadioGroup
-                            aria-label="role"
-                            name="role"
-                        >
-                            <FormControlLabel value="teacher" control={<Radio />} label="Teacher" />
-                            <FormControlLabel value="student" control={<Radio />} label="Student" />
+                   <FormControl component="fieldset" sx={{ width: "90%", mt: 3, textAlign: 'left' }} onBlur={handleOnBlur}>
+                       <FormLabel component="legend">Role</FormLabel>
+                       <RadioGroup 
+                           aria-label="role"
+                           name="role"
+                       >
+                           <FormControlLabel value="teacher" control={<Radio />} label="Teacher" onClick={controlRadio}/>
+                           <FormControlLabel value="student" control={<Radio />} label="Student" />
 
-                        </RadioGroup>
-                    </FormControl>
-                    <br />
-                    <Button variant="contained" type="submit" sx={{ mt: 3 }}>Register</Button>
-                </form>
-                <Typography variant="caption" display="block" gutterBottom sx={{ mt: 1, textAlign: 'center' }}>
-                    Already have an account?<NavLink to="/login">Login</NavLink>
-                </Typography>
-                {user?.email && <Alert severity="success">User Created successfully!</Alert>}
-                {authError && <Alert severity="error">{authError}</Alert>}
+                       </RadioGroup>
+                   </FormControl>
+                   <TextField
+                       id="teacher_initial"
+                       name="teacher_initial"
+                       label="Your Initial"
+                       variant="standard"
+                       sx={{ width: "90%", mt: 2 }}
+                       onBlur={handleOnBlur}
+                   />
+                   <br />
+                   <Button variant="contained" sx={{ mt: 3, px: 9, bgcolor: "#0EA5E9" }} type="submit">Register</Button>
+               </form>
+                    <Typography variant="caption" display="block" gutterBottom sx={{ mt: 2, textAlign: 'center' }}>
+                            Already have an account?<NavLink to="/login">Login</NavLink>
+                        </Typography>
+                        {user?.email && <Alert severity="success">User Created successfully!</Alert>}
+                        {authError && <Alert severity="error">{authError}</Alert>}
+                    </Grid>
+                    
+
+                </Grid>
+
+
             </Container >
         </div>
     );
